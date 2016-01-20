bd1e1c57b5c15bab894529edc437cc3fc79a0553
Puccini, Edward <epuccini@gmx.de>
2016-01-20 17:42:13 +0100

    Final version. Everything fine.

--- make.lisp
+++ make.lisp
@@ -9,7 +9,7 @@
 ; Compile this file and every other needed file gets compiled.
 ; On error check path in compile-files
 ; -------------------------------------------------------------
-(defparameter *make-config* "")
+(defparameter *make-conf* "")
 (defparameter *main-function* nil)
 (defparameter *categories* nil)
 (defparameter *sources* '())
@@ -17,11 +17,46 @@
 
 ; -------------------------------------------------------------
 
+
+(defmacro build-files (directory &rest forms)
+"Macro for load and compile. Just list some files and they
+get compiled and loaded."
+  `(progn
+    ,@(loop for f in forms collect 
+			`(load (compile-file (merge-pathnames ,directory ,f))))))
+
+
+(defun quickload-list (systems)
+"Macro to quickload a system."
+  (mapc #'ql:quickload systems)
+  (print "Systems loaded..."))
+
+
+(defun require-list (packages)
+  "Function require a list of packages."
+  (mapc #'require packages)
+  (print "Packages required..."))
+
+
 (defun load-config ()
   (let ((content 
 		 (with-open-file (stream *make-filename* :direction :input)
 						 (read stream))))
 	content))
+
+(defun setup ()
+  "This function has side effects and sets the
+globals with valid configuration properties."
+  (setq *make-filename* (merge-pathnames 
+						 *default-pathname-defaults* "make.conf"))
+  (format t "filename '~A' set...~%" *make-filename*)
+  (setq *make-conf* (load-config))
+  (format t "Config read...~%")
+  (setq *categories* (getf *make-conf* :categories))
+  (mapc #'pprint *categories*)
+  (setq *main-function* (getf *make-conf* :main))
+  (terpri))
+  
 
 (defun build-category (category)
   (declare (keyword category))
@@ -34,15 +69,18 @@
    (error (condition)
 		  (format t "Error! ~A." condition))))
 
+
 (defun build ()
+  (format t "Building ~A...~%" (getf *make-conf* :application))
   (loop for category in *categories* do
 		(cond ((getf *categories* category) 
 			   (format t "Building category ~A.~%" category)
 			   (build-category category)))))
 
+
 #+sbcl 
 (defun save-bin (&optional (cmp-flag t))
-  (save-lisp-and-die (getf *make-config* :application) 
+  (save-lisp-and-die (getf *make-conf* :application) 
 					 :executable t 
 					 :compression cmp-flag 
 					 :toplevel *main-function*))
@@ -57,38 +95,27 @@
   (declare (ignore cmp-flag)))
   ;;(c:build-program *application*))
 
+
 (defun build-bin ()
   (build)
   (save-bin))
 
-(defmacro build-files (directory &rest forms)
-"Macro for load and compile. Just list some files and they
-get compiled and loaded."
-  `(progn
-    ,@(loop for f in forms collect 
-			`(load (compile-file (merge-pathnames ,directory ,f))))))
 
 (eval-when (:load-toplevel :compile-toplevel :execute)
   "Executed at compile time. Does load and compile
 all necessary files including packages.
 Compile all files on C-c C-k in emacs/slime"
-  (print "Make startup...")
-  (setq *make-filename* (merge-pathnames 
-						 *default-pathname-defaults* "make.conf"))
-  (format t "filename '~A' set...~%" *make-filename*)
-  (setq *make-config* (load-config))
-  (format t "Config read...~%")
-  (mapc #'ql:quickload (getf *make-config* :systems))
-  (print "Systems loaded...")
 
-  (format t "Building ~A...~%" (getf *make-config* :application))
-  (setq *main-function* (getf *make-config* :main))
-  (print "Main function found...")
-  (setq *categories* (getf *make-config* :categories))
-  (mapc #'pprint *categories*)
-  (terpri)
+  (print "Make startup...")
+  ;; Load configuration
+  (setup)
+  ;; Quickload systems
+  (quickload-list (getf *make-conf* :systems))
+  ;; Require packages
+  (require-list (getf *make-conf* :packages))
+  ;; compile everything
   (build)
-  (format t "Building  complete. Starting ~A:" (getf *make-config* :application))
+  (format t "Building  complete. Starting ~A:" (getf *make-conf* :application))
   (cond (*main-function*
 		 (funcall (symbol-function (find-symbol (string-upcase *main-function*)))))))
 
