1d8cd5b72395df36cf55c6900c4ff4914cec756a
Edward Puccini <edward@Edwards-MacBook-Air.local>
2019-03-15 06:49:24 +0100

    Remove tabs

--- src/example.lisp
+++ src/example.lisp
@@ -22,9 +22,9 @@
 (defun squares (x)
   "Multiply 100 times."
   (let ((result 0))
-	(loop for cnt from 0 to 99 do
-		 (setf result (+ result (* x x))))
-	result))
+    (loop for cnt from 0 to 99 do
+         (setf result (+ result (* x x))))
+    result))
 
 (defun main ()
   (terpri)
@@ -46,142 +46,142 @@
   ;; available formatter: ascii-, html-, xml-, one-to-one-formatter
   ;;
   (let ((lg (make-bare-logger 
-			 :writers (list #'error-writer #'file-writer)
-			 :formatter #'ascii-formatter)))
+             :writers (list #'error-writer #'file-writer)
+             :formatter #'ascii-formatter)))
 
-	;; Set default logger with new created logger
-	(set-default-logger lg)
+    ;; Set default logger with new created logger
+    (set-default-logger lg)
 
-	;; simple log output
-	;; to default logger
-	(wrn "First custom-log output")
-	;; custom logger
-	(wrn lg "First default-log output 2+4=" (+ 2 4))
+    ;; simple log output
+    ;; to default logger
+    (wrn "First custom-log output")
+    ;; custom logger
+    (wrn lg "First default-log output 2+4=" (+ 2 4))
 
-	;; simple log output formatted
-	(dbg lg "Second custom log output with format values: " 666 " inbetween " 999)
+    ;; simple log output formatted
+    (dbg lg "Second custom log output with format values: " 666 " inbetween " 999)
 
-	;; trace demo function 'squares' and send ouput to 
-	;; error-writer and file-writer. Use ascii-formatter.
-	(trace-fn 'squares "Trace fn ")
+    ;; trace demo function 'squares' and send ouput to 
+    ;; error-writer and file-writer. Use ascii-formatter.
+    (trace-fn 'squares "Trace fn ")
 
-	;; trigger tracing
-	(squares 2)
-	(squares 4)
-	(squares 8)
+    ;; trigger tracing
+    (squares 2)
+    (squares 4)
+    (squares 8)
 
-	;; cleanup and reset to old fn
-	(untrace-fn 'squares)
+    ;; cleanup and reset to old fn
+    (untrace-fn 'squares)
 
-	;; log a function body
-	(exp-log :dbg "Log expression:"
-					   '(mapcar (lambda (x) (squares x)) 
-							   (append '(1 2 3 4 5) '(4 3 2 1))))
+    ;; log a function body
+    (exp-log :dbg "Log expression:"
+                       '(mapcar (lambda (x) (squares x)) 
+                               (append '(1 2 3 4 5) '(4 3 2 1))))
 
-	
-	;; clear history - we dont want the first logs in our html file
-	(set-history '(""))
-	
-	;; create a custom logger with a socket-writer and a new template-string
-	;; uncomment socket-writer only if you want to behave as client
-	(setq lg (make-logger :writers (list #'htmlfile-writer)
-										;#'email-writer)
-						  :formatter #'html-formatter
-						  :template "[$MACHINE-TYPE]-$TIME-[$LEVEL]-$MESSAGE"))
-	
-	;; First output with email - not successfully tested
-	(wrn lg "Switching to log-level: " (set-log-level :dbg))
-	(dbg lg "Testing new format template.")
-	(inf lg "Testing new format template.")
-	(wrn lg "Testing new format template.")
+    
+    ;; clear history - we dont want the first logs in our html file
+    (set-history '(""))
+    
+    ;; create a custom logger with a socket-writer and a new template-string
+    ;; uncomment socket-writer only if you want to behave as client
+    (setq lg (make-logger :writers (list #'htmlfile-writer)
+                                        ;#'email-writer)
+                          :formatter #'html-formatter
+                          :template "[$MACHINE-TYPE]-$TIME-[$LEVEL]-$MESSAGE"))
+    
+    ;; First output with email - not successfully tested
+    (wrn lg "Switching to log-level: " (set-log-level :dbg))
+    (dbg lg "Testing new format template.")
+    (inf lg "Testing new format template.")
+    (wrn lg "Testing new format template.")
 
-	;; close file
-	(finalize-html)
-	
-	;; get history with: (get-history)
-	
-	;; setup new format string (in clisp there is a problem with
-	;; software-type and -version. They return values, but they are
-	;; mixed with data from a different source)"
-	
-	;; -------------------------------------------------------
-	;; Attention! If you like to test the socke-writer feature
-	;; you have to quickload the USOCKET library and uncomment
-	;; #'socket-writer line and the starting and stopping 
-	;; of the log-server:
+    ;; close file
+    (finalize-html)
+    
+    ;; get history with: (get-history)
+    
+    ;; setup new format string (in clisp there is a problem with
+    ;; software-type and -version. They return values, but they are
+    ;; mixed with data from a different source)"
+    
+    ;; -------------------------------------------------------
+    ;; Attention! If you like to test the socke-writer feature
+    ;; you have to quickload the USOCKET library and uncomment
+    ;; #'socket-writer line and the starting and stopping 
+    ;; of the log-server:
 
-	;; start upd-server only if you want to behave as server
-	;;#+(or sbcl ccl)
-	;; (start-log-server)
+    ;; start upd-server only if you want to behave as server
+    ;;#+(or sbcl ccl)
+    ;; (start-log-server)
 
-	(setq lg (make-logger :writers (list #'error-writer 
-;;										 #'socket-writer
-										 #'rotating-log-writer)
-						  :formatter #'ascii-formatter
-						  :template "[$MACHINE-TYPE]-$TIME-[$LEVEL]-$MESSAGE"))
+    (setq lg (make-logger :writers (list #'error-writer 
+;;                                         #'socket-writer
+                                         #'rotating-log-writer)
+                          :formatter #'ascii-formatter
+                          :template "[$MACHINE-TYPE]-$TIME-[$LEVEL]-$MESSAGE"))
 
-	;; We are testing log-levels now:
-	;; set logging level to 
-	(wrn lg "Switching to log-level: " (set-log-level :dbg))
-	(dbg lg "DEBUG log-output.")
-	(inf lg "INFORMATION log-output.")
-	(wrn lg "WARNING log-output.")
+    ;; We are testing log-levels now:
+    ;; set logging level to 
+    (wrn lg "Switching to log-level: " (set-log-level :dbg))
+    (dbg lg "DEBUG log-output.")
+    (inf lg "INFORMATION log-output.")
+    (wrn lg "WARNING log-output.")
 
-	;; set logging level to TEST
-	(wrn lg "Switching to log-level: " (set-log-level :tst))
-	(dbg lg "DEBUG log-output.")
-	(inf lg "INFORMATION log-output.")
-	(wrn lg "WARNING log-output.")
+    ;; set logging level to TEST
+    (wrn lg "Switching to log-level: " (set-log-level :tst))
+    (dbg lg "DEBUG log-output.")
+    (inf lg "INFORMATION log-output.")
+    (wrn lg "WARNING log-output.")
 
-	;; set logging level to PRODUCTION
-	(wrn lg "Switching to log-level: " (set-log-level :prd))
-	(dbg lg "DEBUG log-output.")
-	(inf lg "INFORMATION log-output.")
-	(wrn lg "WARNING log-output.")
+    ;; set logging level to PRODUCTION
+    (wrn lg "Switching to log-level: " (set-log-level :prd))
+    (dbg lg "DEBUG log-output.")
+    (inf lg "INFORMATION log-output.")
+    (wrn lg "WARNING log-output.")
 
-	;; set log-level debug
-	(set-log-level :dbg)
-	(dbg lg "Back to default loger")
-	
-	;; Back to default logger
+    ;; set log-level debug
+    (set-log-level :dbg)
+    (dbg lg "Back to default loger")
+    
+    ;; Back to default logger
 
-	;; test multithreaded
-	(async 
-	 (progn
-	   (dbg "Async-out thread A! Debug...")
-	   (inf "Async-out thread A! Information...")
-	   (wrn "Async-out thread A! Warning...")))
-	(async 
-	 (progn
-	   (dbg "Async-out thread B! Debug...")
-	   (inf "Async-out thread B! Information...")
-	   (wrn "Async-out thread B! Warning...")))
-	
-	(async 
-	 (progn (inf "I'm going to sleep...") 
-			(sleep 1)
-			(inf "Zzzzzzz...")
-			(sleep 1)
-			(inf "Zzzzzzz...")
-			(sleep 1)
-			(inf "Zzzzzzz...")
-			(sleep 1)
-			(inf "I've slept for " 3 " seconds.")))
-	
-	;; set new loggers
-	(let ((stack-depth 4))
-		  ;; log stack trace with depth 4
-		  (stack :inf stack-depth 
-					 "Stack-trace depth: " stack-depth "~%"))
-	  
-	;; Log memory usage
-	(capture :inf #'room "Memory output:~%")
+    ;; test multithreaded
+    (async 
+     (progn
+       (dbg "Async-out thread A! Debug...")
+       (inf "Async-out thread A! Information...")
+       (wrn "Async-out thread A! Warning...")))
+    (async 
+     (progn
+       (dbg "Async-out thread B! Debug...")
+       (inf "Async-out thread B! Information...")
+       (wrn "Async-out thread B! Warning...")))
+    
+    (async 
+     (progn (inf "I'm going to sleep...") 
+            (sleep 1)
+            (inf "Zzzzzzz...")
+            (sleep 1)
+            (inf "Zzzzzzz...")
+            (sleep 1)
+            (inf "Zzzzzzz...")
+            (sleep 1)
+            (inf "I've slept for " 3 " seconds.")))
+    
+    ;; set new loggers
+    (let ((stack-depth 4))
+          ;; log stack trace with depth 4
+          (stack :inf stack-depth 
+                     "Stack-trace depth: " stack-depth "~%"))
+      
+    ;; Log memory usage
+    (capture :inf #'room "Memory output:~%")
 
-	;; Load shell command output
-	(sys :inf "ps -e | grep sbcl" "Calling shell-command and log output...~%")
+    ;; Load shell command output
+    (sys :inf "ps -e | grep sbcl" "Calling shell-command and log output...~%")
 
-	;; turn off udp-server by killing thread - no socket-logging works
-	;;#+(or sbcl ccl)
-	;; (stop-log-server)
+    ;; turn off udp-server by killing thread - no socket-logging works
+    ;;#+(or sbcl ccl)
+    ;; (stop-log-server)
 
-	(princ "STOP logging!")))
+    (princ "STOP logging!")))
--- src/test.lisp
+++ src/test.lisp
@@ -22,61 +22,61 @@
 (in-package :flood-test)
 
 (defvar *lg* (flood:make-logger :writers (list #'htmlfile-writer
-											   #'file-writer)
-						  :formatter #'html-formatter
-						  :template "[$MACHINE-TYPE]-$TIME-[$LEVEL]-$MESSAGE"))
+                                               #'file-writer)
+                          :formatter #'html-formatter
+                          :template "[$MACHINE-TYPE]-$TIME-[$LEVEL]-$MESSAGE"))
 
 (define-test-case test-make-datetime-string nil "Make a datetime string"
   (multiple-value-bind (date time) (flood:make-datetime-string)
-	(let ((res1 (if (= (length date) 10) t nil))
-		  (res2 (if (= (length time) 8) t nil)))
-	  (and res1 res2))))
+    (let ((res1 (if (= (length date) 10) t nil))
+          (res2 (if (= (length time) 8) t nil)))
+      (and res1 res2))))
 
 (define-test-case test-make-day-string nil "Make a day string"
   (let ((str (make-day-string)))
-	(> (length str) 0)))
+    (> (length str) 0)))
 
 (define-test-case test-copy-file nil "Test if file is copied successfully"
   (flood:dbg *lg* "Test entry")
   (flood:copy-file-from-to
    (concatenate 'string
-				(getf flood:*global-config* :LOG_FILE_NAME)
-				".log")
+                (getf flood:*global-config* :LOG_FILE_NAME)
+                ".log")
    (concatenate 'string
-				(getf flood:*global-config* :LOG_FILE_NAME) ".log.bak"))
+                (getf flood:*global-config* :LOG_FILE_NAME) ".log.bak"))
   (probe-file (concatenate 'string
-				(getf flood:*global-config* :LOG_FILE_NAME) ".log.bak")))
+                (getf flood:*global-config* :LOG_FILE_NAME) ".log.bak")))
 
 (define-test-case test-move-file nil "Test if file is moved successfully"
   (flood:move-file-from-to
    (concatenate 'string
-				(getf flood:*global-config* :LOG_FILE_NAME)
-				".log.bak")
+                (getf flood:*global-config* :LOG_FILE_NAME)
+                ".log.bak")
    (concatenate 'string
-				(getf flood:*global-config* :LOG_FILE_NAME)
-				".log.bak2"))
+                (getf flood:*global-config* :LOG_FILE_NAME)
+                ".log.bak2"))
   (probe-file (concatenate 'string
-				(getf flood:*global-config* :LOG_FILE_NAME)
-				".log.bak2")))
+                (getf flood:*global-config* :LOG_FILE_NAME)
+                ".log.bak2")))
 
 (define-test-case test-file-size nil "Test if file size is read successfully"
   (flood:dbg *lg* "Test entry")
   (let ((size 
-	 (flood:file-size
-	  (concatenate 'string
-		       (getf flood:*global-config* :LOG_FILE_NAME)
-		       ".log"))))
+     (flood:file-size
+      (concatenate 'string
+               (getf flood:*global-config* :LOG_FILE_NAME)
+               ".log"))))
     size))
 
 (define-test-case test-backup-file nil "Test if backup of file is successful"
   (flood:dbg *lg* "Test entry")
   (flood:backup-file
    (concatenate 'string
-				(getf flood:*global-config* :LOG_FILE_NAME) ".log"))
+                (getf flood:*global-config* :LOG_FILE_NAME) ".log"))
   (probe-file (concatenate 'string
-				(getf flood:*global-config* :BACKUP_LOCATION)
+                (getf flood:*global-config* :BACKUP_LOCATION)
                                 (getf flood:*global-config* :LOG_FILE_NAME) 
-				".log.bak")))
+                ".log.bak")))
 
 (define-test-case test-set-history nil "Test if history is set right"
   (flood:set-history '())
@@ -91,7 +91,7 @@
 
 (define-test-case test-get-history nil "Test if history is written"
   (let* ((content (flood:get-history))
-		 (size (length content)))
+         (size (length content)))
     (>= size 3)))
 
 (define-test-case test-append-to-history nil "Test if append to history is fine"
@@ -100,37 +100,37 @@
 
 (define-test-case test-file-writer nil "Test if file writer writes file"
   (let ((lg (flood:make-bare-logger :writers (list #'file-writer)
-									:formatter #'ascii-formatter))
-		(to (concatenate 'string 
-						 (getf flood:*global-config* :LOG_FILE_NAME)
-						 ".log")))
-	(wrn lg "Test")
-	(probe-file to)))
+                                    :formatter #'ascii-formatter))
+        (to (concatenate 'string 
+                         (getf flood:*global-config* :LOG_FILE_NAME)
+                         ".log")))
+    (wrn lg "Test")
+    (probe-file to)))
 
 (define-test-case test-html-writer nil "Test if htmlfile writer writes htmlfile"
   (let ((lg (flood:make-bare-logger :writers (list #'htmlfile-writer)
-									:formatter #'ascii-formatter))
-		(to (concatenate 'string 
-						 (getf flood:*global-config* :HTML_FILE_NAME)
-						 ".html")))
-	(wrn lg "Test")
-	(probe-file to)))
+                                    :formatter #'ascii-formatter))
+        (to (concatenate 'string 
+                         (getf flood:*global-config* :HTML_FILE_NAME)
+                         ".html")))
+    (wrn lg "Test")
+    (probe-file to)))
 
 (defun flood-test::main ()
   (test (test-case "flood library"
-				   (test-case "flood utility functions"
-					 '(test-make-datetime-string)
-					 '(test-make-day-string)
-					 '(test-file-size)
-					 '(test-copy-file)
-					 '(test-move-file)
-					 '(test-backup-file))
-				   (test-case "flood logging functions"
-					 '(test-set-history)
-					 '(test-write-log)
-					 '(test-get-history)
-					 '(test-append-to-history)
-					 '(test-file-writer)
-					 '(test-html-writer)))))
+                   (test-case "flood utility functions"
+                     '(test-make-datetime-string)
+                     '(test-make-day-string)
+                     '(test-file-size)
+                     '(test-copy-file)
+                     '(test-move-file)
+                     '(test-backup-file))
+                   (test-case "flood logging functions"
+                     '(test-set-history)
+                     '(test-write-log)
+                     '(test-get-history)
+                     '(test-append-to-history)
+                     '(test-file-writer)
+                     '(test-html-writer)))))
 
 (flood-test:main)
